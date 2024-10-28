501372fc35ed6578cd50c420bb87527cff0ddd41
SarthakBhardwajCode <85218961+sarthakbhardwaj27@users.noreply.github.com>
2024-10-28 23:43:26 +0530

    initialized the FE along with dark mode from tailwindcss

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('toggleDarkMode').addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  console.log('button clicked')
+});
+
+// Rest of your code...
+console.log('Hello from javscript file')
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: 'selector', // Enable class-based dark mode
+  theme: {
+      extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
